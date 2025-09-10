96e4fbf2e0c6eb11b370204277bdc55e800e4e6f
Saroyar-Islam-Sakib <sisakib232@gmail.com>
2025-09-10 23:51:06 +0600

    Assignment 2 done

---
+++ src/DevSkill.Blog/DevSkill.Blog.Web/Program.cs
@@ -0,0 +1,74 @@
+using DevSkill.Blog.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+// BootstrapLogger Configuration
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("Logs/web-log-.log",rollingInterval:RollingInterval.Day)
+    .CreateBootstrapLogger();
+
+
+try
+{
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    //Serilog Configuration
+    #region Serilog Configuration
+    builder.Host.UseSerilog((context, lc) => lc
+        .MinimumLevel.Debug()
+        .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(builder.Configuration)
+    );
+    #endregion
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+    builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+    builder.Services.AddControllersWithViews();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    app.MapRazorPages()
+       .WithStaticAssets();
+
+    app.Run();
+}
+catch(Exception ex)
+{
+    Log.Fatal(ex, "Application crashed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
