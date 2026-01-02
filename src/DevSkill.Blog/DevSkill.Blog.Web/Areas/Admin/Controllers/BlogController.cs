d73cec8dab005743d2560c1c4ff40c4ddf884a6e
saroyarislamsakib <sisakib232@gmail.com>
2026-01-03 05:29:27 +0600

    Theme integration done Create blogs successfully

---
+++ src/DevSkill.Blog/DevSkill.Blog.Web/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,62 @@
+﻿using Cortex.Mediator;
+using DevSkill.Blog.Application.Features.Blogs.Commands;
+using DevSkill.Blog.Domain.Entities;
+using DevSkill.Blog.Infrastructure.Extensions;
+using DevSkill.Blog.Web.Areas.Admin.Models;
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevSkill.Blog.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        public BlogController(ILogger<BlogController> logger, IMediator mediator, IMapper mapper)
+        {
+            _logger = logger;
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(CreateBlogModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var command = _mapper.Map<BlogPostAddCommand>(model);
+                    var result = await _mediator.SendCommandAsync<BlogPostAddCommand,BlogPost>(command);
+
+                    TempData.Put("ResponseMessage", new ResponseModel
+                    {
+                        Message = "Blog Post Create Successfully",
+                        Response = ResponseTypes.success
+                    });
+                    return RedirectToAction("Create");
+                }
+                catch(Exception ex)
+                {
+                    ModelState.AddModelError("Error", "Failed to create Blog Post");
+                    TempData.Put("ResponseMessage", new ResponseModel
+                    {
+                        Message = "Failed to create Blog Post",
+                        Response = ResponseTypes.danger
+                    });
+
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ src/DevSkill.Blog/DevSkill.Blog.Web/Areas/Admin/Models/CreateBlogModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSkill.Blog.Web.Areas.Admin.Models
+{
+    public class CreateBlogModel
+    {
+        [Required(ErrorMessage ="Please write a Title")]
+        public string Title { get; set; } = null!;
+        [Required(ErrorMessage = "Please write a blog")]
+        public string? Body { get; set; }
+    }
+}
