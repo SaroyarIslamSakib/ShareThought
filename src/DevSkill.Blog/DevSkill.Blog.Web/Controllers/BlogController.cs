9790a1aa9f5e407d6188647e2493d40cf59f1186
saroyarislamsakib <sisakib232@gmail.com>
2026-02-16 20:20:23 +0600

    Login modal close button fixed

--- src/DevSkill.Blog/DevSkill.Blog.Web/Controllers/BlogController.cs
+++ src/DevSkill.Blog/DevSkill.Blog.Web/Controllers/BlogController.cs
@@ -1,14 +1,18 @@
 ﻿using Cortex.Mediator;
+using DevSkill.Blog.Application.Features.BlogsArea.Commands;
 using DevSkill.Blog.Application.Features.BlogsArea.Queries;
 using DevSkill.Blog.Application.Features.Posts.Queries;
 using DevSkill.Blog.Domain;
 using DevSkill.Blog.Domain.Dtos;
 using DevSkill.Blog.Domain.Entities;
+using DevSkill.Blog.Domain.Utilities;
+using DevSkill.Blog.Infrastructure.Extensions;
 using DevSkill.Blog.Infrastructure.Identity;
 using DevSkill.Blog.Web.Areas.Admin.Models;
 using DevSkill.Blog.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Web;
 namespace DevSkill.Blog.Web.Controllers
 {
@@ -17,15 +21,19 @@ namespace DevSkill.Blog.Web.Controllers
         private readonly ILogger<BlogController> _logger;
         private readonly IMediator _mediator;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IServerTime _serverTime;
+
 
         public BlogController(
             ILogger<BlogController> logger,
             IMediator mediator,
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager,
+            IServerTime serverTime)
         {
             _logger = logger;
             _mediator = mediator;
             _userManager = userManager;
+            _serverTime = serverTime;
         }
         public IActionResult Index()
         {
@@ -128,6 +136,91 @@ namespace DevSkill.Blog.Web.Controllers
             {
                 _logger.LogError(ex, "Error in GetPostsJsonData");
                 return Json(DataTables.EmptyResult);
+            }
+        }
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateBlogAreaModel model)
+        {
+            try
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                    return Unauthorized();
+
+                var query = new GetBlogAreaByUserIdQuery
+                {
+                    UserId = user.Id
+                };
+
+                var hasBlog = await _mediator
+                    .SendQueryAsync<GetBlogAreaByUserIdQuery, BlogArea>(query);
+
+                if (hasBlog != null)
+                {
+                    TempData.Put("ResponseMessage", new ResponseModel
+                    {
+                        Message = "You already have a blog",
+                        Response = ResponseTypes.danger
+                    });
+
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+
+                var command = new AddBlogAreaCommand
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    CreatedAt = _serverTime.DateTime,
+                    UserId = user.Id,
+                    UserName = user.FirstName + " " + user.LastName
+                };
+
+                await _mediator
+                    .SendCommandAsync<AddBlogAreaCommand, BlogArea>(command);
+
+                TempData.Put("ResponseMessage", new ResponseModel
+                {
+                    Message = "Blog created successfully",
+                    Response = ResponseTypes.success
+                });
+                await _userManager.AddToRoleAsync(user, "Blogger");
+
+                return RedirectToAction("Index");
+            }
+            catch (InvalidOperationException ex)
+            {
+                // Business rule violation
+                TempData.Put("ResponseMessage", new ResponseModel
+                {
+                    Message = ex.Message,
+                    Response = ResponseTypes.danger
+                });
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (DbUpdateException)
+            {
+                // DB constraint (Unique / FK)
+                TempData.Put("ResponseMessage", new ResponseModel
+                {
+                    Message = "You already have a blog (database constraint).",
+                    Response = ResponseTypes.danger
+                });
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception)
+            {
+                // Unknown error
+                TempData.Put("ResponseMessage", new ResponseModel
+                {
+                    Message = "Something went wrong. Please try again later.",
+                    Response = ResponseTypes.danger
+                });
+
+                return RedirectToAction("Index", "Home");
             }
         }
     }
